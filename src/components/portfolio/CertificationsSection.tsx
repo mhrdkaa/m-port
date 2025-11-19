7a6f637cc7b79af3c4797c8d721f9a78dc6f4ff5
utatau <putramahardika276@gmail.com>
2025-11-19 15:20:51 +0800

    cihuys

--- src/components/portfolio/CertificationsSection.tsx
+++ src/components/portfolio/CertificationsSection.tsx
@@ -3,7 +3,6 @@
 import { Icon } from "@iconify/react";
 import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
-import Link from "next/link";
 import { useState } from "react";
 import SectionHeader from "./SectionHeader";
 
